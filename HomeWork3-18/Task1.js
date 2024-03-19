258ff8fb64bcbd1d34f952e7e0ee369a12117cf2
gintas3073 <157696822+gintas3073@users.noreply.github.com>
2024-03-19 08:27:35 +0200

    pataisymas

--- HomeWork3-18/Task1.js
+++ HomeWork3-18/Task1.js
@@ -1,6 +1,6 @@
-let a = Number(proces.arg[2]);
-let b = Number(proces.arg[3]);
-let c = Number(proces.arg[4]);
+let a = Number(process.arg[2]);
+let b = Number(process.arg[3]);
+let c = Number(process.arg[4]);
 
 
 let s = (a + b + c) / 2;
