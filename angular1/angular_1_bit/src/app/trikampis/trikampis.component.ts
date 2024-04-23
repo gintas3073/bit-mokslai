58aee8689643a98f11aa5103a548cedf7c458e4a
gintas3073 <157696822+gintas3073@users.noreply.github.com>
2024-04-23 19:05:48 +0300

    angularcont

--- angular1/angular_1_bit/src/app/trikampis/trikampis.component.ts
+++ angular1/angular_1_bit/src/app/trikampis/trikampis.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-trikampis',
-  standalone: true,
-  imports: [],
-  templateUrl: './trikampis.component.html',
-  styleUrl: './trikampis.component.css'
+  selector: 'app-triangle',
+  templateUrl: './triangle.component.html',
+  styleUrls: ['./triangle.component.css']
 })
-export class TrikampisComponent {
+export class TriangleComponent {
+  side1: number;
+  side2: number;
+  side3: number;
+  area: number = null;
 
+  calculateArea() {
+    const s = (this.side1 + this.side2 + this.side3) / 2;
+    this.area = Math.sqrt(s * (s - this.side1) * (s - this.side2) * (s - this.side3));
+  }
 }
